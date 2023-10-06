fbd937231b5695d330d1ef8163e90f1e8305c97c
Brooksolomon <86517756+Brooksolomon@users.noreply.github.com>
2023-10-06 16:58:08 +0300

    Update Pancake-Sorting.py

--- Pancake-Sorting.py
+++ Pancake-Sorting.py
@@ -1,13 +1,16 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
-        n = len(arr)
-        new=[]
-        while n > 1:
-            maxi = max(arr[:n])
-            x= arr.index(maxi)
-            new.append(x+1)
-            new.append(n)
-            arr = list(reversed(arr[:x+1]))  + arr[x+1:]
-            arr = list(reversed(arr[:n])) + arr [n:]
-            n-=1
-        return new
+        keyer = sorted(arr)
+        p = len(arr) -1
+        ans = []
+        for i in range(p+1):
+            x = arr.index(keyer[p])
+            arr = list(reversed(arr[:x+1])) + arr[x+1:]
+            arr = list(reversed(arr[:p+1]) )+ arr[p+1:]
+            ans.append(x+1)
+            ans.append(p+1)
+            p-=1
+        return ans
+        
+
+
