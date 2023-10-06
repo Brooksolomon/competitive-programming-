acfd26e9c89ca9545cb1b833971e6ed44dce6182
Brooksolomon <86517756+Brooksolomon@users.noreply.github.com>
2023-10-06 16:37:10 +0300

    Update Merge-intervals.py

--- Merge-intervals.py
+++ Merge-intervals.py
@@ -1,16 +1,13 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda a : a [0])
-
-        answer=[intervals[0]]
-
-        for inter in intervals[1:]:
-            if  inter[0]  <= answer[-1][1]:
-                answer[-1][1]=max(inter[1],answer[-1][1])
-            else :
-                answer.append([inter[0],inter[1]])
-
-
-
-
-        return answer
+        intervals = sorted(intervals, key = lambda a:a[0])
+        p=0
+        n = len(intervals)
+        while p < n-1:
+            if intervals[p][1] >= intervals[p+1][0]:
+                intervals[p][1] = max(intervals[p+1][1],intervals[p][1])
+                intervals.pop(p+1)
+                n-=1
+            else:
+                p+=1
+        return intervals
