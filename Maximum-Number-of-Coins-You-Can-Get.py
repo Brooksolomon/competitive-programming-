658ef72b34850aeb67ab232c75757c90840a6e68
Brooksolomon <86517756+Brooksolomon@users.noreply.github.com>
2023-10-06 17:01:20 +0300

    Update Maximum-Number-of-Coins-You-Can-Get.py

--- Maximum-Number-of-Coins-You-Can-Get.py
+++ Maximum-Number-of-Coins-You-Can-Get.py
@@ -1,5 +1,8 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
-        piles.sort(reverse=True)
-        lst = piles[1:len(piles) - int(len(piles) /3) :2]
-        return sum(lst)
+        piles.sort(reverse = True)
+        total = 0
+        for i in range(1,len(piles)-(len(piles))//3,2):
+            total+=piles[i]
+        return total
+        
