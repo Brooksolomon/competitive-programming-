89c6c19fc947648de790476c6ef7c3c63ecfd6b0
Brooksolomon <86517756+Brooksolomon@users.noreply.github.com>
2023-10-18 04:58:55 +0300

    Update Repeated-DNA-Sequences.py

--- Repeated-DNA-Sequences.py
+++ Repeated-DNA-Sequences.py
@@ -1,1 +1,17 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        l=-1
+        r = 8
+        md = {}
+        ans = set()
+
+        while r< len(s):
+            r+=1
+            l+=1
+            if md.get(s[l:r+1],None):
+                ans.add(s[l:r+1])
+            else:
+                md[s[l:r+1]] =  1
+        return ans
+
 
