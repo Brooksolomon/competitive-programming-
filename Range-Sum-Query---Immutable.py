e120594b31c6461d97f86851cf27c5a5533093b4
Brooksolomon <86517756+Brooksolomon@users.noreply.github.com>
2023-10-19 19:18:51 +0300

    Update Range-Sum-Query---Immutable.py

--- Range-Sum-Query---Immutable.py
+++ Range-Sum-Query---Immutable.py
@@ -1,14 +1,15 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.nums=nums
-        for i in range(1,len(nums)):
-            self.nums[i] = self.nums[i] + self.nums[i-1]
+        self.presum = [0] *(len(nums) + 1)
+
+        for i in range(1,len(nums)+1):
+            self.presum[i] = self.presum[i-1] + nums[i-1]
+        
+
     def sumRange(self, left: int, right: int) -> int:
-        if left==0:
-            return self.nums[right]
-        else:
-            return self.nums[right] - self.nums[left-1]
+        return self.presum[right+1] - self.presum[left]
+
         
 
 
