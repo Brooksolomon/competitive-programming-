b3bf88ae38a6bc4470ea6bd440c697e896d839f9
Brooksolomon <86517756+Brooksolomon@users.noreply.github.com>
2023-10-21 13:12:37 +0300

    Update Valid-parenthesis.py

--- Valid-parenthesis.py
+++ Valid-parenthesis.py
@@ -1,41 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        opener = list()
-        count = -1
-
-        check = True
-        for elem in s:
-            if elem =="(" or elem=="[" or elem=="{":
-                opener.append(elem)
-                count +=1
-            else:
-                if count > -1:
-                    if elem==")":
-                        if opener[count] == "(":
-                            opener.pop(count)
-                            count-=1
-                        else:
-                            check = False
-                            break
-
-                    elif elem=="]":
-                        if opener[count] == "[":
-                            opener.pop(count)
-                            count -= 1
-                        else:
-                            check = False
-                            break
-                    elif elem=="}":
-                        if opener[count] == "{":
-                            opener.pop(count)
-                            count -= 1
-                        else:
-                            check = False
-                            break
+        stack=[]
+        md ={']':'['
+            ,'}':'{',
+            ')':'('}
+        openers = set({'(','{','['})
+        for i in s:
+            if i in openers:
+                stack.append(i)
+            elif stack:
+                if stack[-1] == md[i]:
+                    stack.pop()
                 else:
-                    check = False
-        if len(opener) !=0:
-            check = False
-
-        
-        return check
+                    return False 
+            else:
+                return False
+        if stack:
+            return False 
+        return True
