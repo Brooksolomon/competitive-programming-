713addb77571e2532a592853b5f12cf63b0af968
Brooksolomon <86517756+Brooksolomon@users.noreply.github.com>
2023-11-12 14:34:01 +0300

    Update Fizz-buzz.py

--- Fizz-buzz.py
+++ Fizz-buzz.py
@@ -1,13 +1,13 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        answer = []
-        for i in range(1, n+1):
-            answer.insert(i,str(i))
-        for i in range(1, n+1):
-            if i % 3 == 0:
-                answer[i - 1] = "Fizz"
-            if i % 5 ==0:
-                answer[i - 1] = "Buzz"
-            if i % 3 == 0 and i % 5 == 0 :
-                answer[i - 1] = "FizzBuzz"
-        return answer
+        stack = []
+        for i in range(1,n+1):
+            if i%3==0 and i%5==0:
+                stack.append('FizzBuzz')
+            elif i%3==0:
+                stack.append('Fizz')
+            elif i%5==0:
+                stack.append('Buzz')
+            else:
+                stack.append(str(i))
+        return stack
