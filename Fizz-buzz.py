22272da089119f8a65fc37d004d2135575a1c5ed
Brooksolomon <86517756+Brooksolomon@users.noreply.github.com>
2024-01-31 19:34:08 +0300

    Update Fizz-buzz.py

--- Fizz-buzz.py
+++ Fizz-buzz.py
@@ -1,13 +1,14 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        stack = []
+        ans = []
         for i in range(1,n+1):
             if i%3==0 and i%5==0:
-                stack.append('FizzBuzz')
+                ans.append("FizzBuzz")
             elif i%3==0:
-                stack.append('Fizz')
-            elif i%5==0:
-                stack.append('Buzz')
+                ans.append("Fizz")
+            elif i%5 == 0:
+                ans.append("Buzz")
             else:
-                stack.append(str(i))
-        return stack
+                ans.append(str(i))
+            
+        return ans
