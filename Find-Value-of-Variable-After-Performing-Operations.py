d74ededc89de5435d065626edf1753c0ef59d563
Brooksolomon <86517756+Brooksolomon@users.noreply.github.com>
2024-02-02 22:39:46 +0300

    Update Find-Value-of-Variable-After-Performing-Operations.py

--- Find-Value-of-Variable-After-Performing-Operations.py
+++ Find-Value-of-Variable-After-Performing-Operations.py
@@ -2,7 +2,7 @@ class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
         x = 0 
         for i in operations :
-            if i[0] =='-' or i[2] =='-':
+            if i[1] =='-':
                 x-=1
             else:
                 x+=1
