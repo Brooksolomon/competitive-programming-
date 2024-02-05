2ce6d56c7e4c7787d83f8328b1e0b7aa92fc9081
Brooksolomon <86517756+Brooksolomon@users.noreply.github.com>
2024-02-05 22:23:35 +0300

    Update Nested-Lists.py

--- Nested-Lists.py
+++ Nested-Lists.py
@@ -6,18 +6,14 @@ if __name__ == '__main__':
         result.append([name,score])
     result.sort(key = lambda x : (x[1],x[0]))
     found = False
-    
-    ans = []
     for i in result:
         if not found:
             if i[1]!=result[0][1]:
                 key = i[1]
-                ans.append(i[0])
+                print(i[0])
                 found = True
         else:
             if i[1] == key:
-                ans.append(i[0])
+                print(i[0])
             else:
                 break
-    for i in ans:
-        print(i)
