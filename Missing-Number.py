aa554db8879d1481a2673e62f74c574a8aa25455
Brooksolomon <86517756+Brooksolomon@users.noreply.github.com>
2024-02-08 22:08:06 +0300

    Update Missing-Number.py

--- Missing-Number.py
+++ Missing-Number.py
@@ -1,18 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        
-        left , right = 0 , len(nums) - 1 
-        nums.sort()
-        if nums[-1] == len(nums)-1:
-            return len(nums)
-        if nums[0]!=0:return 0
-        while left <=right:
-            m = left + (right - left) // 2
+        n = set(nums)
+        for i in range(len(nums)+1):
+            if i not in n:
+                return i
 
-            if nums[m] < m:
-                return m
-            elif nums[m] == m:
-                left=m+1
-            else :
-                right = m - 1
-        return left
+        
